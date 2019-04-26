d7f4a94f626c81668908c73ed6e74b75378466e4
fansi-sifan <sifan1121@gmail.com>
2019-04-26 18:45:11 -0400

    move readxl_online function to new file

---
+++ R/readxl_online.R
@@ -0,0 +1,33 @@
+# read excel files from url
+
+readxl_online <- function(url, type = NULL, ...) {
+  test <- stringr::str_detect(url, "[.]xls|[.]zip")
+  if (test == FALSE) {
+    print(message("Expecting file extension of type .xlsx, .xls or .zip. Check the URL or the data source for the correct file extension and use the type argument"))
+  }
+  # test for individual file extensions for xls use look forward, xls not
+  # followed by x
+  t1 <- stringr::str_detect(url, "[.]xlsx")
+  t2 <- stringr::str_detect(url, "[.]xls(?!x)")
+  tz <- stringr::str_detect(url, "[.]zip")
+  if (t1 == TRUE) {
+    type = ".xlsx"
+  }
+  if (t2 == TRUE) {
+    type = ".xls"
+  }
+  if (tz == TRUE) {
+    httr::GET(url, httr::write_disk("tmp.zip", overwrite = TRUE))
+    tmp <- unzip("tmp.zip")
+    # On osx more than one file name is returned, select first element.
+    df <- readxl::read_excel(tmp[[1]],...)
+    return(df)
+  }
+  if (!is.null(type)) {
+    type = type
+
+  }
+  df <- httr::GET(url, httr::write_disk(paste0("tmp", type), overwrite = TRUE))
+  df <- readxl::read_excel(paste0("tmp", type),...)
+
+}
