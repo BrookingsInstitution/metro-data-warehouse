a7efc4647a2f6a76033ee0b0e7fb58c1ce0cfe83
fansi-sifan <sifan1121@gmail.com>
2019-04-26 12:18:58 -0400

    script to prepare county_cbsa_st

--- data-raw/county_cbsa_st.R
+++ data-raw/county_cbsa_st.R
@@ -111,8 +111,39 @@ update.county_cbsa_st <- function(url, cbp_year, acs_year){
 
 }
 
+# read excel files from url
+readxl_online <- function(url, type = NULL, ...) {
+  test <- stringr::str_detect(url, "[.]xls|[.]zip")
+  if (test == FALSE) {
+    print(message("Expecting file extension of type .xlsx, .xls or .zip. Check the URL or the data source for the correct file extension and use the type argument"))
+  }
+  # test for individual file extensions for xls use look forward, xls not
+  # followed by x
+  t1 <- stringr::str_detect(url, "[.]xlsx")
+  t2 <- stringr::str_detect(url, "[.]xls(?!x)")
+  tz <- stringr::str_detect(url, "[.]zip")
+  if (t1 == TRUE) {
+    type = ".xlsx"
+  }
+  if (t2 == TRUE) {
+    type = ".xls"
+  }
+  if (tz == TRUE) {
+    httr::GET(url, write_disk("tmp.zip", overwrite = TRUE))
+    tmp <- unzip("tmp.zip")
+    # On osx more than one file name is returned, select first element.
+    df <- readxl::read_excel(tmp[[1]],...)
+    return(df)
+  }
+  if (!is.null(type)) {
+    type = type
 
+  }
+  df <- httr::GET(url, write_disk(paste0("tmp", type), overwrite = TRUE))
+  df <- readxl::read_excel(paste0("tmp", type),...)
+
+}
 # UPDATE!
 county_cbsa_st <- update.county_cbsa_st(url, cbp_year, acs_year)
 
-usethis::use_data("county_cbsa_st")
+usethis::use_data(county_cbsa_st)
