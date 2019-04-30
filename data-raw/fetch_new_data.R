6faaeaada9999e910ab9cb4415704143695785c6
fansi-sifan <sifan1121@gmail.com>
2019-04-30 17:19:04 -0400

    functions to get naics data

--- data-raw/fetch_new_data.R
+++ data-raw/fetch_new_data.R
@@ -50,3 +50,29 @@ get_county_urban_rural <- function(url){
     rename(pct.urban.county = POPPCT_URBAN)
 }
 
+get_naics <- function(url){
+  tmp <- readxl_online(url,skip=2)%>%
+    # remove columns contain only nas
+    select_if(~sum(!is.na(.)) > 0)
+
+  names(tmp) <- c("code.naics","name.naics")
+  return(tmp)
+}
+
+get_naics_17_12<- function(url){
+  tmp <- readxl_online(url, skip = 2,col_type = 'text')%>%
+    # remove columns contain only nas
+    select_if(~sum(!is.na(.)) > 0)
+
+  names(tmp) <- c("code.naics6.2017","name.naics6.2017","code.naics6.2012","name.naics6.2012")
+  return(tmp)
+}
+
+get_clustermapping <- function(url){
+  tmp <- readxl_online(url,sheet = "NAICS")
+  names(tmp) <- c("code.naics6.all","code.naics6.2012","code.naics6.2007","code.naics6.2002","code.naics6.1997",
+                  "name.naics6.all", "traded.naics6",
+                  "code.cluster.naics6","name.cluster.naics6","code.subcluster.naics6","name.subcluster.naics6")
+  return(tmp)
+
+}
