9acd9669ecc5fe2c8894c112d4b1bc191dd9afe7
fansi-sifan <sifan1121@gmail.com>
2019-05-10 15:30:35 -0400

    overwrite=T

--- data-raw/county_pres_votes.R
+++ data-raw/county_pres_votes.R
@@ -1,9 +1,10 @@
 ## code to prepare `county_pres_votes` dataset goes here
 county_pres_votes <- read.csv("V:/Sifan/R/xwalk/county vote history.csv")
 
-library(tidyverse)
+library(dplyr)
+library(stringr)
 
 county_pres_votes <- county_pres_votes%>%
   mutate(code.county = str_pad(fip, 5, "left", pad = "0"))
 
-usethis::use_data(county_pres_votes)
+usethis::use_data(county_pres_votes, overwrite = T)
