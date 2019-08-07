e6740654b7bda16da2cf75f2b612127debadd1ed
David Harshbarger <DHarshbarger@brookings.edu>
2019-08-08 07:04:01 +1100

    split original file into 2 (helper functions and computations), and added section to write shapefiles

--- data-raw/create_basic_geometries.R
+++ data-raw/create_basic_geometries.R
@@ -16,11 +16,18 @@ library(sf)
 library(mapview)
 library(readxl)
 
-homedir <- "V:/David/data warehouse sandbox"
-setwd(homedir)
+# homedir <- "V:/David/data warehouse sandbox"
+# setwd(homedir)
 
 # read in top 100 metros
-top100 <- read_excel("top100metros.xlsx")
+load("data/county_cbsa_st.rda")
+top100 <- county_cbsa_st %>%
+  filter(cbsa_type == "top100") %>%
+  select(cbsa_code, cbsa_name) %>%
+  distinct()
+
+# source functions for cleaning up tigris shapes and shifting AK/HI geometries
+source("data-raw/create_basic_geometries_helper.R")
 
 # when tigris downloads geographies they extend over water boundaries
 # we will first import a "nation" shape that outlines the coast and other borders
@@ -48,41 +55,23 @@ territories <- tibble::tribble(
   78,   "VI"
 )
 
+
 # load crs info of census shapefiles (4269)
-load("crs_NAD83.RData")
+load("data-raw/crs_NAD83.RData")
+crs_basic <- "+proj=longlat +datum=NAD83 +no_defs"
 
 # transform nation shape to match the crs of the states (the govt default)
 nation_sf <- nation_sf %>%
   st_transform(crs = crs_NAD83)
 
+
+
 ##============================================================================##
 # IMPORT STATES, COUNTIES, METROS SHAPES
 ##============================================================================##
 
-# first, define functions that will be repeatedly used to filter and clip
-cleanup_tigris <- function(shp){
-  shp %>%
-
-    # filter for 50 states + DC
-    filter(!(STATEFP %in% territories$st_code)) %>%
-
-    # clip to national extent
-    st_intersection(nation_sf$geometry)
-}
-
-# this one is for cbsa, which cross state lines
-cleanup_tigris2 <- function(shp){
-  shp %>%
-
-    # filter out Pureto Rico
-    filter(!grepl(x = NAME,
-                  pattern = ", PR")) %>%
-
-    # clip to national extent
-    st_intersection(nation_sf$geometry)
-}
-
 # sf refers to spatial data, ld/hd refers to low definition / high definition
+# cleanup functions from helper script
 states51_sf_ld <- tigris::states(class = "sf", cb = TRUE) %>%
   cleanup_tigris()
 states51_sf_hd <- tigris::states(class = "sf", cb = FALSE) %>%
@@ -106,9 +95,9 @@ metros51_sf_ld <- cbsas51_sf_ld %>%
   filter(GEOID %in% metros51_sf_hd$GEOID)
 
 top100_sf_ld <- metros51_sf_ld %>%
-  inner_join(top100, by = c("GEOID" = "CBSA"))
+  inner_join(top100, by = c("GEOID" = "cbsa_code"))
 top100_sf_hd <- metros51_sf_hd %>%
-  inner_join(top100, by = c("GEOID" = "CBSA"))
+  inner_join(top100, by = c("GEOID" = "cbsa_code"))
 
 
 ##============================================================================##
@@ -148,211 +137,266 @@ metros49_sf_hd <- metros51_sf_hd %>%
 # where matrix A is used to flatten, scale and/or rotate, and b to translate x
 # more info at https://cran.r-project.org/web/packages/sf/vignettes/sf3.html
 
-# define rotation function
-# (this is not used below, but is included for potential future use)
-rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
 
-# only need low-def versions, as inset maps are not used for geoprocessing
-HI_sf <- states51_sf_ld %>%
+HI_sf_ld <- states51_sf_ld %>%
   filter(STATEFP == "15") %>%
   st_transform(3857) %>%
   st_buffer(dist = 0)
 
-AK_sf <- states51_sf_ld %>%
+HI_sf_hd <- states51_sf_hd %>%
+  filter(STATEFP == "15") %>%
+  st_transform(3857) %>%
+  st_buffer(dist = 0)
+
+AK_sf_ld <- states51_sf_ld %>%
   filter(STATEFP == "02") %>%
   st_transform(3857) %>%
   st_buffer(dist = 0)
 
+AK_sf_hd <- states51_sf_hd %>%
+  filter(STATEFP == "02") %>%
+  st_transform(3857) %>%
+  st_buffer(dist = 0)
 
-# make a function to shift all HI or AK geometries the same way
-shift_AKorHI <- function(shape, AKorHI, rescale = NULL){
-
-  # test validity of supplied arguments
-  if (is.na(st_crs(shape))) stop("'shape' must have crs set")
-  if (!is.character(AKorHI)) stop("'AKorHI' must be character, one of 'AK', or 'HI'")
-
-  # prepare the sf object
-  prepared_shape <- shape %>%
-    st_transform(3857) %>%
-    st_buffer(dist = 0)
-
-  # define AK and HI default centroids in this projection
-  AK_centroid <- c(-16968368, 9610108)
-  HI_centroid <- c(-17405371, 2303492)
-
-
-  #### cases for default scaling of HI and AK ####
-
-  # HAWAII:
-  if (tolower(AKorHI) %in% c("hi", "hawaii")) {
-
-    # use the centroid coords to translate the shape to the origin
-    # this prevents unwanted translation when scaling
-    # then rescale at the origin
-    # then add centroid back to translate to original location, now smaller
-    if (missing(rescale)){
-      rescale <- 1.0
-    }
-    shape_scaled_sfc <- (prepared_shape$geometry - HI_centroid) * rescale + HI_centroid
-
-    # replace the geometry with the new scaled geometry
-    shape_scaled <- prepared_shape
-    shape_scaled$geometry <- shape_scaled_sfc
-
-    HI_shift_coords <- c(6000000, 400000)
-    shape_scaled$geometry <- shape_scaled$geometry + HI_shift_coords
-    shape_transformed <- shape_scaled %>%
-      st_set_crs(3857)
-  }
-
-  # ALASKA:
-  # this gets weird because the archipelago crosses 180 degrees, so
-  # typical affine transformations don't work as expected
-  # need to split geometries into two
-
-  if (tolower(AKorHI) %in% c("ak", "alaska")) {
-
-    if (missing(rescale)){
-      rescale <- 0.2
-    }
-
-    # define a function to convert WKT to boxes as sf
-    st_text_to_sf <- function(x){
-      x %>%
-        st_as_sfc() %>%
-        st_set_crs(4269) %>%
-        st_sf()
-    }
-
-    AK_box_right <- st_text_to_sf(
-      "POLYGON((-179.999999999 73,-115 73,-115 40,-179.999999999 40,-179.999999999 73))))"
-      )
-    AK_box_left <- st_text_to_sf(
-      "POLYGON((149 73,179.999999999 73,179.999999999 40,149 40,149 73))"
-      )
-    AK_boxes <- rbind(AK_box_right, AK_box_left) %>%
-      st_as_sf() %>%
-      mutate(box = c("right", "left")) %>%
-      select(box, geometry) %>%
-      st_transform(crs = st_crs(prepared_shape))
-
-    prepared_shape_int <- prepared_shape %>%
-      # split in two by the boxes
-      st_intersection(AK_boxes)
-
-    # shift the right-side piece to the origin, by subtracting the centroid coords
-    AK_right_sfc <- (prepared_shape_int %>% filter(box == "right"))$geometry - AK_centroid
-    # this strips the coordinate info, so replace it
-    AK_right_sfc <- st_set_crs(AK_right_sfc, st_crs(prepared_shape_int$geometry))
-
-    # shift the left-side piece to the origin
-    # need coordinates that describe a -360 rotation in this projection:
-    wrap360_epsg3857 <- c(-40052752.78, 0)
-    AK_left_sfc <- (prepared_shape_int %>% filter(box == "left"))$geometry + wrap360_epsg3857 - AK_centroid
-    AK_left_sfc <- st_set_crs(AK_left_sfc, st_crs(prepared_shape_int$geometry))
-
-    # with both pieces of AK now at the origin, union them into one MULTIPOLYGON
-    AK_origin <- prepared_shape_int %>%
-      mutate(geometry = c(AK_right_sfc, AK_left_sfc)) %>%
-      select(-box) %>%
-      # group by everything EXCEPT geometry
-      group_by_at(vars(-geometry)) %>%
-      summarise() %>%
-      ungroup()
-
-
-    # by default shrink to 20%, then add centroid coords back to put the shrunk shape back
-    shape_scaled <- (AK_origin$geometry * rescale + AK_centroid) %>%
-      st_set_crs(st_crs(prepared_shape$geometry))
-
-    # define the arbitrary shift coordinates that will determine where AK shows up on the map
-    AK_shift_coords <- c(4400000, -6400000)
-
-    shape_scaled <- (shape_scaled + AK_shift_coords) %>%
-      st_set_crs(st_crs(prepared_shape$geometry)) %>%
-      st_buffer(dist = 0)
-
-    shape_transformed <- AK_origin %>%
-      mutate(geometry = shape_scaled)
-  }
-
-  warning("'shift_AKorHI' changes coordinates reference system to epsg 3857, measured in meters.")
-  return(shape_transformed)
-}
-
-# use the shift function defined above to create low-def insets of AK and HI
-HI_inset_sf <- HI_sf %>%
+# use the shift function defined in the helper script to create insets of AK and HI
+HI_inset_ld <- HI_sf_ld %>%
   shift_AKorHI(AKorHI = "HI")
 
-AK_inset_sf <- AK_sf %>%
+HI_inset_hd <- HI_sf_hd %>%
+  shift_AKorHI(AKorHI = "HI")
+
+AK_inset_ld <- AK_sf_ld %>%
   shift_AKorHI(AKorHI = "AK")
 
-# inspect visually with ggplot
-p <- ggplot() +
-  geom_sf(data = continentalus_sf %>% st_transform(3857)) +
-  geom_sf(data = HI_inset_sf) +
-  geom_sf(data = AK_inset_sf)
+AK_inset_hd <- AK_sf_hd %>%
+  shift_AKorHI(AKorHI = "AK")
 
+
+# inspect visually with ggplot
+# p <- ggplot() +
+#   geom_sf(data = continentalus_sf %>% st_transform(3857)) +
+#   geom_sf(data = HI_inset_sf) +
+#   geom_sf(data = AK_inset_sf)
+
+states51_inset_ld <- rbind(states49_sf_ld,
+                           AK_inset_ld %>% st_transform(crs = crs_NAD83),
+                           HI_inset_ld %>% st_transform(crs = crs_NAD83))
 
 # use mapview to check the appearance
 # mapview(continentalus_sf) + mapview(AK_inset_sf) + mapview(HI_inset_sf)
 
 # use ggplot to check the apperance in an equal-area projection
-p + coord_sf(crs = 102009)
+# p + coord_sf(crs = 102009)
 
 
 ##============================================================================##
 # APPLY INSET TRANSFORMATIONS TO COUNTIES, CBSAS, AND METROS
 ##============================================================================##
 
-# unlike state insets, sub-state inset geographies are to be high def
-
 countiesHI_inset_hd <- counties51_sf_hd %>%
   filter(str_sub(GEOID, end = 2L) == "15") %>%
   shift_AKorHI(AKorHI = "HI") %>%
   st_transform(crs = crs_NAD83)
+countiesHI_inset_ld <- counties51_sf_ld %>%
+  filter(str_sub(GEOID, end = 2L) == "15") %>%
+  shift_AKorHI(AKorHI = "HI") %>%
+  st_transform(crs = crs_NAD83)
+
 
 countiesAK_inset_hd <- counties51_sf_hd %>%
+  filter(str_sub(GEOID, end = 2L) == "02") %>%
+  shift_AKorHI(AKorHI = "AK") %>%
+  st_transform(crs = crs_NAD83)
+countiesAK_inset_ld <- counties51_sf_ld %>%
   filter(str_sub(GEOID, end = 2L) == "02") %>%
   shift_AKorHI(AKorHI = "AK") %>%
   st_transform(crs = crs_NAD83)
 
 counties51_inset_hd <- rbind(counties49_sf_hd,
                              countiesAK_inset_hd,
-                             countiesHI_inset_hd)
+                             countiesHI_inset_hd) %>%
+  st_transform(crs = crs_NAD83)
+counties51_inset_ld <- rbind(counties49_sf_ld,
+                             countiesAK_inset_ld,
+                             countiesHI_inset_ld) %>%
+  st_transform(crs = crs_NAD83)
 
 cbsasHI_inset_hd <- cbsas51_sf_hd %>%
   filter(grepl(x = NAME, pattern = ", HI")) %>%
-  shift_AKorHI(AKorHI = "HI")
+  shift_AKorHI(AKorHI = "HI") %>%
+  st_transform(crs = crs_NAD83)
+cbsasHI_inset_ld <- cbsas51_sf_ld %>%
+  filter(grepl(x = NAME, pattern = ", HI")) %>%
+  shift_AKorHI(AKorHI = "HI") %>%
+  st_transform(crs = crs_NAD83)
 
 cbsasAK_inset_hd <- cbsas51_sf_hd %>%
   filter(grepl(x = NAME, pattern = ", AK")) %>%
-  shift_AKorHI(AKorHI = "AK")
+  shift_AKorHI(AKorHI = "AK") %>%
+  st_transform(crs = crs_NAD83)
+cbsasAK_inset_ld <- cbsas51_sf_ld %>%
+  filter(grepl(x = NAME, pattern = ", AK")) %>%
+  shift_AKorHI(AKorHI = "AK") %>%
+  st_transform(crs = crs_NAD83)
+
+cbsas51_inset_hd <- rbind(cbsas49_sf_hd,
+                          cbsasAK_inset_hd,
+                          cbsasHI_inset_hd) %>%
+  st_transform(crs = crs_NAD83)
+cbsas51_inset_ld <- rbind(cbsas49_sf_ld,
+                          cbsasAK_inset_ld,
+                          cbsasHI_inset_ld) %>%
+  st_transform(crs = crs_NAD83)
 
 metrosHI_inset_hd <- metros51_sf_hd %>%
   filter(grepl(x = NAMELSAD, pattern = ", HI Metro Area")) %>%
-  shift_AKorHI(AKorHI = "HI")
+  shift_AKorHI(AKorHI = "HI") %>%
+  st_transform(crs = crs_NAD83)
+metrosHI_inset_ld <- metros51_sf_ld %>%
+  filter(GEOID %in% metrosHI_inset_hd$GEOID) %>%
+  shift_AKorHI(AKorHI = "HI") %>%
+  st_transform(crs = crs_NAD83)
 
 metrosAK_inset_hd <- cbsas51_sf_hd %>%
   filter(grepl(x = NAMELSAD, pattern = ", AK Metro Area")) %>%
-  shift_AKorHI(AKorHI = "AK")
+  shift_AKorHI(AKorHI = "AK") %>%
+  st_transform(crs = crs_NAD83)
+metrosAK_inset_ld <- cbsas51_sf_ld %>%
+  filter(GEOID %in% metrosAK_inset_hd$GEOID) %>%
+  shift_AKorHI(AKorHI = "AK") %>%
+  st_transform(crs = crs_NAD83)
+
+metros51_inset_hd <- rbind(metros49_sf_hd,
+                           metrosAK_inset_hd,
+                           metrosHI_inset_hd) %>%
+  st_transform(crs = crs_NAD83)
+metros51_inset_ld <- rbind(metros49_sf_ld,
+                           metrosAK_inset_ld,
+                           metrosHI_inset_ld) %>%
+  st_transform(crs = crs_NAD83)
+
 
 # split and merge
 top100_inset_hd <- rbind(metrosAK_inset_hd,
                          metrosHI_inset_hd) %>%
-  inner_join(top100, by = c("GEOID" = "CBSA")) %>%
+  inner_join(top100, by = c("GEOID" = "cbsa_code")) %>%
   rbind(top100_sf_hd %>%
           filter(!(GEOID %in% (rbind(metrosAK_inset_hd,
-                                     metrosHI_inset_hd))$GEOID)) %>%
-          st_transform(crs = 3857))
+                                     metrosHI_inset_hd))$GEOID))) %>%
+  st_transform(crs = crs_NAD83)
+top100_inset_ld <- rbind(metrosAK_inset_ld,
+                         metrosHI_inset_ld) %>%
+  inner_join(top100, by = c("GEOID" = "cbsa_code")) %>%
+  rbind(top100_sf_ld %>%
+          filter(!(GEOID %in% (rbind(metrosAK_inset_ld,
+                                     metrosHI_inset_ld))$GEOID))) %>%
+  st_transform(crs = crs_NAD83)
 
 
+##============================================================================##
+# WRITE OUTPUT FILES
+##============================================================================##
+
+##====================================##
+# basic geometries (no insets)
+##====================================##
+
+## STATES
+# hd
+st_write(obj = states51_sf_hd, dsn = "data/states51_hd.shp")
+st_write(obj = states49_sf_hd, dsn = "data/states49_hd.shp")
+# ld
+st_write(obj = states51_sf_ld, dsn = "data/states51_ld.shp")
+st_write(obj = states49_sf_ld, dsn = "data/states49_ld.shp")
+
+## COUNTIES
+# hd
+st_write(obj = counties51_sf_hd, dsn = "data/counties51_hd.shp")
+st_write(obj = counties49_sf_hd, dsn = "data/counties49_hd.shp")
+# ld
+st_write(obj = counties51_sf_ld, dsn = "data/counties51_ld.shp")
+st_write(obj = counties49_sf_ld, dsn = "data/counties49_ld.shp")
+
+## CBSAS
+# hd
+st_write(obj = cbsas51_sf_hd, dsn = "data/cbsas51_hd.shp")
+st_write(obj = cbsas49_sf_hd, dsn = "data/cbsas49_hd.shp")
+# ld
+st_write(obj = cbsas51_sf_ld, dsn = "data/cbsas51_ld.shp")
+st_write(obj = cbsas49_sf_ld, dsn = "data/cbsas49_ld.shp")
+
+## METROS
+# hd
+st_write(obj = metros51_sf_hd, dsn = "data/metros51_hd.shp")
+st_write(obj = metros49_sf_hd, dsn = "data/metros49_hd.shp")
+# ld
+st_write(obj = metros51_sf_ld, dsn = "data/metros51_ld.shp")
+st_write(obj = metros49_sf_ld, dsn = "data/metros49_ld.shp")
+
+## TOP 100 METROS (no continental version, as HI has a top100 metro in Honolulu)
+# hd
+st_write(obj = top100_sf_hd, dsn = "data/top100_hd.shp")
+# ld
+st_write(obj = top100_sf_ld, dsn = "data/top100_ld.shp")
+
+## ALASKA AND HAWAII
+#hd
+st_write(obj = AK_sf_hd %>% st_transform(crs = crs_NAD83), dsn = "data/AK_hd.shp")
+st_write(obj = HI_sf_hd %>% st_transform(crs = crs_NAD83), dsn = "data/HI_hd.shp")
+
+# ld
+st_write(obj = AK_sf_ld %>% st_transform(crs = crs_NAD83), dsn = "data/AK_ld.shp")
+st_write(obj = HI_sf_ld %>% st_transform(crs = crs_NAD83), dsn = "data/HI_ld.shp")
 
 
+##====================================##
+# inset geometries
+##====================================##
 
-# test
-p +
-  geom_sf(data = top100_inset_hd %>% st_centroid(),
-          col = "red",
-          size = 3) +
-  coord_sf(crs = 102009)
+## COUNTIES INSETS
+# hd
+st_write(obj = countiesAK_inset_hd, dsn = "data/countiesAK_inset_hd.shp")
+st_write(obj = countiesHI_inset_hd, dsn = "data/countiesHI_inset_hd.shp")
+st_write(obj = counties51_inset_hd, dsn = "data/counties51_inset_hd.shp")
+# ld
+st_write(obj = countiesAK_inset_ld, dsn = "data/countiesAK_inset_ld.shp")
+st_write(obj = countiesHI_inset_ld, dsn = "data/countiesHI_inset_ld.shp")
+st_write(obj = counties51_inset_ld, dsn = "data/counties51_inset_ld.shp")
+
+
+## CBSAS INSETS
+# hd
+st_write(obj = cbsasAK_inset_hd, dsn = "data/cbsasAK_inset_hd.shp")
+st_write(obj = cbsasHI_inset_hd, dsn = "data/cbsasHI_inset_hd.shp")
+st_write(obj = cbsas51_inset_hd, dsn = "data/cbsas51_inset_hd.shp")
+# ld
+st_write(obj = cbsasAK_inset_ld, dsn = "data/cbsasAK_inset_ld.shp")
+st_write(obj = cbsasHI_inset_ld, dsn = "data/cbsasHI_inset_ld.shp")
+st_write(obj = cbsas51_inset_ld, dsn = "data/cbsas51_inset_ld.shp")
+
+
+# METROS INSETS
+# hd
+st_write(obj = metrosAK_inset_hd, dsn = "data/metrosAK_inset_hd.shp")
+st_write(obj = metrosHI_inset_hd, dsn = "data/metrosHI_inset_hd.shp")
+st_write(obj = metros51_inset_hd, dsn = "data/metros51_inset_hd.shp")
+# ld
+st_write(obj = metrosAK_inset_ld, dsn = "data/metrosAK_inset_ld.shp")
+st_write(obj = metrosHI_inset_ld, dsn = "data/metrosHI_inset_ld.shp")
+st_write(obj = metros51_inset_ld, dsn = "data/metros51_inset_ld.shp")
+
+
+## TOP 100 METROS INSET
+# hd
+st_write(obj = top100_inset_hd,   dsn = "data/top100_inset_hd.shp")
+# ld
+st_write(obj = top100_inset_ld,   dsn = "data/top100_inset_ld.shp")
+
+
+## ALASKA AND HAWAII INSETS
+# hd
+st_write(obj = AK_inset_hd %>% st_transform(crs = crs_NAD83), dsn = "data/AK_inset_hd.shp")
+st_write(obj = HI_inset_hd %>% st_transform(crs = crs_NAD83), dsn = "data/HI_inset_hd.shp")
+# ld
+st_write(obj = AK_inset_ld %>% st_transform(crs = crs_NAD83), dsn = "data/AK_inset_ld.shp")
+st_write(obj = HI_inset_ld %>% st_transform(crs = crs_NAD83), dsn = "data/HI_inset_ld.shp")
